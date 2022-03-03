6f03415e18754988c03997a13b593bbe1adcbded
Shilpa Benarji <sbenarji@amazon.com>
2022-03-02 17:10:30 -0800

    Fixed the issue with reading the placement data file by providing the right path
    cr: https://code.amazon.com/reviews/CR-65844126

--- src/com/amazonaws/ebsblacksmithservice/dagger/modules/PlacementModule.java
+++ src/com/amazonaws/ebsblacksmithservice/dagger/modules/PlacementModule.java
@@ -5,6 +5,7 @@ import com.amazonaws.ebsblacksmithservice.capacity.CapacityProvider;
 import com.amazonaws.ebsblacksmithservice.capacity.FileReaderCapacityProvider;
 import com.amazonaws.ebsblacksmithservice.placement.RandomizedPlacementStrategy;
 import com.amazonaws.ebsblacksmithservice.placement.PlacementStrategy;
+import com.amazonaws.ebsblacksmithservice.types.Domain;
 import dagger.Module;
 import dagger.Provides;
 
@@ -16,8 +17,8 @@ public class PlacementModule {
     @Provides
     @Singleton
     @Named("FileCapacityProvider")
-    static CapacityProvider provideFileCapacityProvider() {
-        return new FileReaderCapacityProvider("MockPlacementData.json");
+    static CapacityProvider provideFileCapacityProvider(@Named("Root") String root) {
+        return new FileReaderCapacityProvider(root+"/MockPlacementData.json");
     }
 
     @Provides
