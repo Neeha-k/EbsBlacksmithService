09b4f2d1a4ceb70e0c468a201ef94113de7ff5d9
Krishna Mishra <kkm@amazon.com>
2022-11-11 13:24:20 -0800

    Update Targeted pool to have Osprey and Garfield, remove existing Garfield from Target pool

--- src/com/amazonaws/ebsblacksmithservice/placement/PlacementStrategy.java
+++ src/com/amazonaws/ebsblacksmithservice/placement/PlacementStrategy.java
@@ -1,14 +1,14 @@
 package com.amazonaws.ebsblacksmithservice.placement;
 
+import java.util.List;
+
 import com.amazonaws.ebsblacksmithservice.types.MetalDiskInternal;
 import com.amazonaws.ebsblacksmithservice.types.MetalServerInternal;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public interface PlacementStrategy {
-    List<String> TARGETED_ONLY_PLACEMENT_SERVER_ADDRESSES = Collections.unmodifiableList((Arrays.asList("30.135.143.139:5684")));
+    List<String> TARGETED_ONLY_PLACEMENT_SERVER_ADDRESSES = List.of("30.133.7.112:5684", "30.135.143.141:5684");
+
     List<MetalServerInternal> placementServers(PlacementOptions options);
+
     List<MetalDiskInternal> placementDisks(PlacementOptions options);
 }
