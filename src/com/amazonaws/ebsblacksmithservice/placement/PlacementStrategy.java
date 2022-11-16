b2edf8414589fb0795fa242f7bc70d1b589ae7cd
Andrew Sharon <shrndr@amazon.com>
2022-11-16 09:36:45 -0800

    Adds 30.135.143.138 Garfield host to TargetedPlacementOnly Pool
    https://t.corp.amazon.com/V767541466
    
    cr: https://code.amazon.com/reviews/CR-79755588

--- src/com/amazonaws/ebsblacksmithservice/placement/PlacementStrategy.java
+++ src/com/amazonaws/ebsblacksmithservice/placement/PlacementStrategy.java
@@ -6,7 +6,7 @@ import com.amazonaws.ebsblacksmithservice.types.MetalDiskInternal;
 import com.amazonaws.ebsblacksmithservice.types.MetalServerInternal;
 
 public interface PlacementStrategy {
-    List<String> TARGETED_ONLY_PLACEMENT_SERVER_ADDRESSES = List.of("30.133.7.112:5684", "30.135.143.141:5684");
+    List<String> TARGETED_ONLY_PLACEMENT_SERVER_ADDRESSES = List.of("30.133.7.112:5684", "30.135.143.141:5684", "30.135.143.138:5684");
 
     List<MetalServerInternal> placementServers(PlacementOptions options);
 
