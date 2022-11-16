8d8a11876504ba9d0295f751d5b5fbf7a9c9ca7c
Andrew Sharon <shrndr@amazon.com>
2022-11-16 14:15:28 -0800

    Adds OspreyG 30.133.7.124 to TargetedOnlyPlacement
    https://t.corp.amazon.com/V767733233
    
    cr: https://code.amazon.com/reviews/CR-79777088

--- src/com/amazonaws/ebsblacksmithservice/placement/PlacementStrategy.java
+++ src/com/amazonaws/ebsblacksmithservice/placement/PlacementStrategy.java
@@ -9,7 +9,7 @@ public interface PlacementStrategy {
     List<String> TARGETED_ONLY_PLACEMENT_SERVER_ADDRESSES = List.of(
         "30.133.7.112:5684", "30.135.143.141:5684", 
         "30.135.143.138:5684", "30.133.7.104:5684",
-        "30.133.7.86:5684"
+        "30.133.7.86:5684", "30.133.7.124:5684"
     );
 
     List<MetalServerInternal> placementServers(PlacementOptions options);
